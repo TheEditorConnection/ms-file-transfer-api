bd051893c5078d6f4d0b23137226522e3f1afd17
Issai Mendoza <mendozaissai@gmail.com>
2024-10-10 16:07:59 -0600

    feat: add changes from token

--- src/scripts/authToken.ts
+++ src/scripts/authToken.ts
@@ -4,15 +4,15 @@ import { argv } from "process";
 const createAuthToken = async (
     subject: string,
     audience: Audience[],
-    expiresIn: string,
-    grant? : Grant[]
+    expiresIn?: string | null,
+    grant?: Grant[]
 ): Promise<void> => {
     const jwtService = new JWTService()
 
-    console.log(jwtService.sign(subject, audience,expiresIn,grant))
+    console.log(jwtService.sign(subject, audience, expiresIn, grant))
 }
 
 
-// Example npm run auth-token ADMIN admin '30 days'
-createAuthToken(argv[2],argv[3].split(','),argv[4],(argv[5] || '').split(',') as Grant[]);
-
+// npm run auth-token ADMIN admin null
+// npm run auth-token ADMIN admin '30 days'
+createAuthToken(argv[2], argv[3].split(','), argv[4] === 'null' ? null : argv[4], (argv[5] || '').split(',') as Grant[])
