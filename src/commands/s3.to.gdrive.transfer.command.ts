880da2ba01587cf4a884765ac425dea88f59aa83
Issai Mendoza <mendozaissai@gmail.com>
2024-10-30 13:16:11 -0600

    feat: add detailed transaction logging to S3ToGDriveTransferCommand
    - Integrated Logger with transaction-specific logging for `S3ToGDriveTransferCommand`
    - Logged start, file name determination, upload success, and time taken for the S3 to Google Drive transfer process
    - Enhanced error handling by implementing `handleError` for consistent logging and notification of failures
    - Recorded notification payload and processing time per transaction for improved monitoring and debugging

--- src/commands/s3.to.gdrive.transfer.command.ts
+++ src/commands/s3.to.gdrive.transfer.command.ts
@@ -5,6 +5,7 @@ import { Config } from '../config/config';
 import { IClientPayload } from '../interfaces/client.payload.interface';
 import { notify } from '../utils/notifier';
 import { handleError } from '../utils/errors/error.handler';
+import { TransferDirection } from '../utils/enums/transfer.direction.enum';
 
 export class S3ToGDriveTransferCommand {
     private payload: IClientPayload;
@@ -29,6 +30,7 @@ export class S3ToGDriveTransferCommand {
         const startTime = new Date();
 
         Logger.info(`Starting process to upload S3 file: ${this.s3FilePath} to Google Drive at ${startTime.toISOString()}`);
+        Logger.createTransactionLogger(this.transferId, `Starting process to upload S3 file: ${this.s3FilePath} to Google Drive`);
 
         try {
             const s3Stream = await this.s3Service.downloadFileAsStream(this.s3FilePath);
@@ -43,22 +45,29 @@ export class S3ToGDriveTransferCommand {
                 throw new Error('Final file name could not be determined.');
             }
 
+            Logger.info(`Final file name determined: ${finalFileName}`);
+            Logger.createTransactionLogger(this.transferId, `Final file name determined: ${finalFileName}`);
+
             const driveFileId = await this.googleDriveService.uploadFileStream(s3Stream, finalFileName, this.googleDriveFolderId);
 
             Logger.info(`File uploaded to Google Drive successfully, Drive File ID: ${driveFileId}`);
+            Logger.createTransactionLogger(this.transferId, `File uploaded to Google Drive successfully, Drive File ID: ${driveFileId}`);
 
-            notify(
-                this.getUrl(),
-                this.getToken(),
-                {
-                    ...this.payload,
-                    googleDriveFileId: driveFileId,
-                    status: 'success'
-                });
+            const payload = {
+                ...this.payload,
+                googleDriveFileId: driveFileId,
+                status: 'success'
+            };
+
+            await notify(this.getUrl(), this.getToken(), payload);
+
+            Logger.createTransactionLogger(this.transferId, `Payload sent to client: ${JSON.stringify(payload)}`);
 
             const endTime = new Date();
             const duration = (endTime.getTime() - startTime.getTime()) / 1000;
+            Logger.logProcessingTime(this.transferId, duration, TransferDirection.S3_TO_GDRIVE);
             Logger.info(`Process completed at ${endTime.toISOString()}, total time: ${duration.toFixed(2)} seconds`);
+            Logger.createTransactionLogger(this.transferId, `Process completed at ${endTime.toISOString()}, total time: ${duration.toFixed(2)} seconds`);
 
         } catch (error) {
             await handleError(error, this.transferId, this.payload, this.getUrl(), this.getToken());
